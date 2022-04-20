d7296666067556e42b3d479e7b7bb70544fc624a
thaonp <thaonpfx15559@funix.edu.vn>
2022-04-20 15:41:15 +0700

    2.3

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const http = require('http')
 const express = require('express')
 const app = express() 
 
-app.use((req, res, next) => { // register mw
+app.use((req, res, next) => { 
     console.log('mw 1');
-    next(); // allow req to go to the next func/mw
+    next(); // allow req to go to the next OF NEXT func/mw
 })
 
 app.use((req, res, next) => {
     console.log('mw 2');
+    res.send('<h1>hello from expjs</h1>') // set default type of content = text/html
 })
 
 const server = http.createServer(app)
