659b85056fa975b46c3308e90bbf493985025df4
thaonp <thaonpfx15559@funix.edu.vn>
2022-04-20 16:45:33 +0700

    2.4

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
-
 const express = require('express')
 const app = express() 
 
-app.use((req, res, next) => { 
-    console.log('mw 1');
-    next(); 
+// app.use(['startOfDomainChar',] callback [, callback(s)])
+app.use('/', (req, res, next) => { // apply to all req
+    console.log('abc');
+    next()
 })
 
-app.use((req, res, next) => {
-    console.log('mw 2');
-    res.send('<h1>hello from expjs</h1>') 
+app.use('/add-product', (req, res, next) => {
+    res.send('<h1> the add product page </h1>') 
+}) // need to register first or it will never be reached bc res => end stream => only take '/' case
+
+app.use('/', (req, res, next) => {
+    res.send('<h1> hello from expjs </h1>') 
 })
 
 app.listen(3000) 
-
-/* behind the scenes :
-    app.listen = function listen() {
-        const server = http.createServer(this) // pass app obj itself
-        return server.listen.apply(server, argument) // take the passed arg
-    }
-*/
