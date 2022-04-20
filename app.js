b5b20040f525533f2c4fc994647d80777c608dc1
thaonp <thaonpfx15559@funix.edu.vn>
2022-04-20 15:20:14 +0700

    2.2

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 
 const http = require('http')
 const express = require('express')
-const app = express() // initialise a new obj that will be stored & managed by expjs
+const app = express() 
+
+app.use((req, res, next) => { // register mw
+    console.log('mw 1');
+    next(); // allow req to go to the next func/mw
+})
+
+app.use((req, res, next) => {
+    console.log('mw 2');
+})
 
 const server = http.createServer(app)
 
