3e5db5deba44b8df8bfa9a241a6383b6192b4130
thaonp <thaonpfx15559@funix.edu.vn>
2022-04-21 15:39:42 +0700

    2.12

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const express = require('express')
 const bodyParser = require('body-parser')
 
@@ -12,7 +14,7 @@ app.use('/admin', adminRoute)
 app.use(shopRoute)
 
 app.use((req,res) => {
-    res.status(404).send('<h1>page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views', '404.html'))
 })
 
 app.listen(3000) 
--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,5 @@ const router = express.Router()
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
 }) 
-// join() -> concat + correct paths
-// __dirname = PATH from current file to the containing directory (the whole pj folder)
 
 module.exports = router
