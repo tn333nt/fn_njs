74d76f11f15ac03cb35e9596b2f140aba17c1a04
thaonp <thaonpfx15559@funix.edu.vn>
2022-04-26 08:22:08 +0700

    5.8 (connect sequelize to db)

--- util/database.js
+++ util/database.js
@@ -1,10 +1,13 @@
-const mysql = require('mysql2')
+const Sequelize = require('sequelize') // constructor f 
 
-const pool = mysql.createPool({ 
-    host: 'localhost',
-    user: 'root',
-    database: 'node-udemy',
-    password: 'thtltcbtst111'
-})
+const sequelize = new Sequelize( // new instance 
+    'node-udemy',
+    'root',
+    'thtltcbtst111',
+    {dialect : mysql}
+)
 
-module.exports = pool.promise() 
+module.exports = sequelize
+
+// execute all sql code behind the scenes 
+// map these code into objects
