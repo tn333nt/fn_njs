8fecc945c8d5f186e1580349f14d31d43a1772be
thaonp <thaonpfx15559@funix.edu.vn>
2022-04-26 18:01:10 +0700

    5.18 (set up relations & store data in req's prop)

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database')
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -16,10 +18,31 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res) => {
+    User.findByPk(1)
+    .then( user => {
+        console.log(user);
+        req.user = user // add new field to req obj with assigned value is a sequelize obj => can use utility methods
+        next()
+    }) // store data in a req
+}) // register cb as mw for icm req
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
 app.use(errorController.get404);
 
-sequelize.sync() 
+Product.belongsTo(User, {
+    // constraints: true,
+    onDelete: 'CASCADE'
+})
+User.hasMany(Product)
+
+sequelize
+    // .sync({ force: true })
+    .sync()
+    .then( () => User.findByPk(1) )
+    .then( user => user ? user : User.create({
+        name: 'abc',
+        email: 'abc@example.com'
+    })) // or return or Promise.resolve()
     .then(() => app.listen(3000))
