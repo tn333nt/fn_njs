67849db866885359db6d0d1f84500123ce3dac29
thaonp <thaonpfx15559@funix.edu.vn>
2022-05-02 15:54:54 +0700

    9.15 (return err page)

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ app.use(passData.passAuthData)
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
+app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
 mongoose
--- middleware/passData.js
+++ middleware/passData.js
@@ -2,20 +2,20 @@ const User = require('../models/user');
 
 exports.passUser = (req, res, next) => {
     console.log(req.session.user)
-    if (!req.session.user) { // find a user without the ss obj existing -> crash app
+    if (!req.session.user) { 
       return next()
     }
     User.findById(req.session.user._id)
     .then(user => { 
-        if(!user) { // -> continue without user INSTEAD if can not find the user
+        if(!user) { 
           return next()
         }
         req.user = user 
         next()
     })
     .catch(err => {
-      throw new Error(err)
-    }); // only fired for technical issues (non-existing users, db is down, ...)
+      res.redirect('/500')
+    })
 }
 
 exports.passAuthData = (req, res, next) => {
@@ -23,15 +23,3 @@ exports.passAuthData = (req, res, next) => {
   res.locals.csrfToken = req.csrfToken() 
   next()
 }
-
-
-
-/** for sync code
- * try {
- *  // try a certain code
- * } catch {
- *  // catch & handle potential err
- * }
- * 
- * ===> make sure other code keep executing even if some err is existing
- */
